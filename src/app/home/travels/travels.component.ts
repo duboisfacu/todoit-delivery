f0b95f07bcfe652a6f179892a4420d04043bf215
duboisfacu <facundubois@hotmail.com>
2021-12-21 22:25:16 -0300

    console log cleaned

--- src/app/home/travels/travels.component.ts
+++ src/app/home/travels/travels.component.ts
@@ -8,79 +8,84 @@ import Swal from 'sweetalert2';
 @Component({
   selector: 'app-travels',
   templateUrl: './travels.component.html',
-  styleUrls: ['./travels.component.sass']
+  styleUrls: ['./travels.component.sass'],
 })
 export class TravelsComponent implements OnInit {
+  public loading = true;
+  public haveTravels = true;
+  public travels!: any;
+  public utc = new Date().toISOString().slice(0, 10);
+  public year = parseInt(this.utc.slice(0, 4));
+  public month = parseInt(this.utc.slice(5, 7));
+  public day = parseInt(this.utc.slice(8, 10));
 
-  public loading = true
-  public haveTravels = true
-  public travels!:any
-  public utc = new Date().toISOString().slice(0, 10)
-  public year = parseInt(this.utc.slice(0, 4))
-  public month = parseInt(this.utc.slice(5, 7))
-  public day = parseInt(this.utc.slice(8, 10))
+  constructor(private tr: TravelsService) {}
 
-  constructor(private tr: TravelsService) { }
+  onClick = (object: any) => {
+    let travelId =
+      object['travelEquipmentDTOs'][object['travelEquipmentDTOs'].length - 1][
+        'equipment'
+      ]['id'];
+    let statusTravel = object['lastStatusTravel'] === 1 ? 2 : 6;
 
-  onClick=(object: any)=>{
+    let userOperation = 2;
+    let cadeteId = JSON.parse(localStorage.getItem('token') || '{}');
+    let isReasigned = false;
+    this.tr
+      .postTravel(travelId, statusTravel, userOperation, cadeteId, isReasigned)
+      .subscribe(
+        (resp) => {
+          Swal.fire({
+            title: 'Aceptado correctamente',
+            icon: 'success',
+            confirmButtonText: 'Continuar',
+            confirmButtonColor: '#FD611A',
+          });
+          this.loadTravels();
+        },
+        (error) => {
+          error.error ===
+          'Cadete supera los 3 viajes en estado entrega asignada o retiro asignado'
+            ? Swal.fire({
+                title: 'Ya has aceptado el máximo de viajes disponibles.',
+                icon: 'error',
+                confirmButtonText: 'Continuar',
+                confirmButtonColor: '#FD611A',
+              })
+            : Swal.fire({
+                title: 'Error inesperado, intenta nuevamente',
+                icon: 'error',
+                confirmButtonText: 'Continuar',
+                confirmButtonColor: '#FD611A',
+              });
+        }
+      );
+  };
 
-    let travelId = object['travelEquipmentDTOs'][object['travelEquipmentDTOs'].length - 1]['equipment']['id']
-    let statusTravel = object['lastStatusTravel'] === 1 ? 2 : 6
+  loadTravels() {
+    let one = this.tr.getTravel(1);
+    let five = this.tr.getTravel(5);
 
-    let userOperation= 2
-    let cadeteId= JSON.parse(localStorage.getItem('token') || '{}')
-    let isReasigned= false
-    console.log(travelId, statusTravel, userOperation, cadeteId, isReasigned)
-    this.tr.postTravel(travelId, statusTravel, userOperation,cadeteId,isReasigned).subscribe(resp => {
-      console.log(resp)
-      Swal.fire({
-        title: 'Aceptado correctamente',
-        icon: 'success',
-        confirmButtonText: 'Continuar',
-        confirmButtonColor: '#FD611A'
-      })
-      this.loadTravels()
-    }, error =>{
-      console.log(error)
-      error.error === "Cadete supera los 3 viajes en estado entrega asignada o retiro asignado" ? 
-        Swal.fire({
-          title: 'Ya has aceptado el máximo de viajes disponibles.',
-          icon: 'error',
-          confirmButtonText: 'Continuar',
-          confirmButtonColor: '#FD611A'
-        })
-      
-      :       
-      Swal.fire({
-        title: 'Error inesperado, intenta nuevamente',
-        icon: 'error',
-        confirmButtonText: 'Continuar',
-        confirmButtonColor: '#FD611A'
-      })
-    })
-  }
+    forkJoin([one, five]).subscribe((results) => {
+      this.travels = [...results[0], ...results[1]];
+      this.loading = false;
+      this.travels.sort(function (a: Travel, b: Travel) {
+        return (
+          Date.parse(
+            a.travelEquipmentDTOs[a.travelEquipmentDTOs.length - 1]
+              .operationDate
+          ) -
+          Date.parse(
+            b.travelEquipmentDTOs[b.travelEquipmentDTOs.length - 1]
+              .operationDate
+          )
+        );
+      });
 
-  loadTravels(){
-    let one = this.tr.getTravel(1)
-    let five = this.tr.getTravel(5)
-
-    forkJoin([one,five])
-    .subscribe(
-      results => {
-      this.travels = [...results[0], ...results[1] ]
-      this.loading = false
-      this.travels.sort(function(a:Travel,b:Travel){
-        return Date.parse(a.travelEquipmentDTOs[a.travelEquipmentDTOs.length -1].operationDate) - Date.parse(b.travelEquipmentDTOs[b.travelEquipmentDTOs.length -1].operationDate)
-      })
-
-      console.log(this.travels)
-      this.haveTravels = this.travels.length > 0 ? true : false
-      console.log(this.haveTravels)
-
+      this.haveTravels = this.travels.length > 0 ? true : false;
     });
   }
   ngOnInit(): void {
-    this.loadTravels()
+    this.loadTravels();
   }
-
 }
